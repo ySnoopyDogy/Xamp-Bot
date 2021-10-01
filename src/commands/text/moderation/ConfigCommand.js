85b1397d212c76bfabfa357bc27df503056bef65
Luxanna <CommandPlayermine@gmail.com>
2021-10-01 17:48:09 -0300

    Feat: Create command to display all settings

--- src/commands/text/moderation/ConfigCommand.js
+++ src/commands/text/moderation/ConfigCommand.js
@@ -1,5 +1,5 @@
 const Command = require("../../../structures/MessageCommand");
-const { MessageEmbed, MessageSelectMenu } = require('discord.js')
+const { MessageEmbed } = require('discord.js')
 
 class MessageConfigCommand extends Command {
   constructor(client) {
@@ -11,57 +11,22 @@ class MessageConfigCommand extends Command {
   }
 
   async run(ctx) {
-    if (!ctx.args || ctx.args.length === 0) {
-      const noArgsEmbed = new MessageEmbed()
-        .setColor('BLURPLE')
-        .setTitle('⚙️ | Escolha o que você quer configurar')
-        .setDescription(`Lembrando que você pode configurar direto usando \`${process.env.PREFIX}config <opção> <valor>\``)
 
+    const allConfigs = await this.client.database.findOne({ name: 'config' })
+    const embed = new MessageEmbed()
+      .setTitle('Configurações')
+      .setDescription(`**Canal de Criação de Ticket PT**: <#${allConfigs.messageTicketBR ?? '`NONE`'}> (${allConfigs.messageTicketBR ?? '`NONE`'})
+      **Canal de Criação de Ticket US**: <#${allConfigs.messageTicketUS ?? '`NONE`'}> (${allConfigs.messageTicketUS ?? '`NONE`'})
+      **Categoria para Criação dos Tickets**: <#${allConfigs.messageTicketCategory ?? '`NONE`'}> (${allConfigs.messageTicketCategory ?? '`NONE`'})     
+      **Canal para os Logs de Tickets**: <#${allConfigs.messageTicketLog ?? '`NONE`'}> (${allConfigs.messageTicketLog ?? '`NONE`'})
+      **Canal para Escolha de Idioma**: <#${allConfigs.messageLang ?? '`NONE`'}> (${allConfigs.messageLang ?? '`NONE`'})
+      **Cargo do Idioma PT**: <@&${allConfigs.roleLangBR ?? '`NONE`'}> (${allConfigs.roleLangBR ?? '`NONE`'})
+      **Cargo do Idioma US**: <@&${allConfigs.roleLangUS ?? '`NONE`'}> (${allConfigs.roleLangUS ?? '`NONE`'})
+      **Cargo de Administradores**: <@&${allConfigs.roleAdministrator ?? '`NONE`'}> (${allConfigs.roleAdministrator ?? '`NONE`'})
+      `)
+      .setColor('DARK_GREEN')
 
-      const selectOption = new MessageSelectMenu()
-        .setCustomId(`${ctx.message.id} | SELECT`)
-        .setPlaceholder('Selecione a Opção')
-        .setMinValues(1)
-        .setMaxValues(1)
-        .addOptions([{
-          label: 'Canal Escolha Idioma',
-          value: 'channel-lang',
-          description: 'O ID do canal responsável por selecionar os idiomas',
-          emoji: '🌐'
-        }, {
-          label: 'ID Cargo Inglês',
-          value: 'role-english',
-          description: 'O ID do cargo que define o idioma do usuário para o Inglês',
-          emoji: '🇺🇸',
-        },
-        {
-          label: 'ID Cargo Portugûes',
-          value: 'role-portuguese',
-          description: 'O ID do cargo que define o idioma do usuário para o português',
-          emoji: '🇧🇷'
-        },
-        {
-          label: 'Canal Ticket BR',
-          value: 'channel-ticket-br',
-          description: 'O ID do canal para a Mensagem do ticket em portugues',
-          emoji: '📗'
-        },
-        {
-          label: 'Canal Ticket US',
-          value: 'channel-ticket-us',
-          description: 'O ID do canal para a Mensagem do ticket em inglês',
-          emoji: '📘'
-        },
-        {
-          label: 'Categoria Tickets',
-          value: 'category-ticket',
-          description: 'O ID da categoria que todos tickets serão criados',
-          emoji: '🗄️'
-        },
-        ])
-
-      ctx.reply({ embeds: [noArgsEmbed], components: [{ type: 1, components: [selectOption] }] })
-    }
+    ctx.reply({ embeds: [embed] })
   }
 }
 
